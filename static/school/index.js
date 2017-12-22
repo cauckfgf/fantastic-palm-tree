deff9ad067282f0d4d27dc2b6b5ea86facd6cc39
fgf <fgf@bimsheng.com>
2017-12-22 17:59:23 +0800

    c

--- static/school/index.js
+++ static/school/index.js
@@ -13,7 +13,8 @@ function initVue() {
                 "http://modelbucket.oss-cn-shanghai.aliyuncs.com/school/full/3d.svf",
             ],
             videosrc: 'rtmp://rtmp.open.ys7.com/openlive/01c8d597e6c8453a9402fedb41e52620.hd',
-            videomobile:'http://hls.open.ys7.com/openlive/01c8d597e6c8453a9402fedb41e52620.m3u8'
+            videomobile:'http://hls.open.ys7.com/openlive/01c8d597e6c8453a9402fedb41e52620.m3u8',
+            build:"教学楼"
         },
         methods:{
             onSelectedCallback0(event) {
@@ -23,8 +24,10 @@ function initVue() {
                 var self = this;
                 if(event.dbIdArray[0]==430 || event.dbIdArray[0]==284){
                     loadInitialModelV1(this.url[0],self.onSelectedCallback0);
+                    self.build = "教学楼";
                 }else if(event.dbIdArray[0]==268 || event.dbIdArray[0]==882){
                     loadInitialModelV1(this.url[1],self.onSelectedCallback0);
+                    self.build = "教职工宿舍";
                 }
                 console.log('构件',event.dbIdArray[0]);
             }
